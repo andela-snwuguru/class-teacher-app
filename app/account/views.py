421c9baa3bb91e43ada697d6c13d1ba50161209e
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-05 09:53:18 +0100

    [Feature] User login

--- app/account/views.py
+++ app/account/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
 
 class Login(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'login.html', {})
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                print("User is valid, active and authenticated")
+                return redirect('/classes/')
+            else:
+                error_message = "The password is valid, but the account has been disabled!"
+        else:
+           error_message = "The username and password were incorrect.";
+        return render(request, 'login.html', {'error_message':error_message})
