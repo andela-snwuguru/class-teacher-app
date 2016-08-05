301eab4798ac10749ece202482c91983683d90c7
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-05 10:43:03 +0100

    [Feature] Implement create and view students

--- app/students/models.py
+++ app/students/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from classes.models import Classes
 
-# Create your models here.
+class Student(models.Model):
+    room = models.ForeignKey(Classes, default=0)
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    created_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.first_name + ' ' + self.last_name
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        ordering = ['-created_date']
+
--- app/students/views.py
+++ app/students/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import View
+from django.http import HttpResponseRedirect, Http404
+from students.forms import StudentForm
+from students.models import Student
 
-# Create your views here.
+class StudentView(View):
+    def get(self, request, *args, **kwargs):
+        students = Student.objects.all()
+        form = StudentForm(request.POST or None)
+        return render(request, 'students.html', {'form': form, 'students': students})
+
+    def post(self, request, *args, **kwargs):
+        form = StudentForm(request.POST or None)
+        if form.is_valid():
+            student = form.save()
+            messages.success(request, 'New Student details successfully added')
+        else:
+            messages.success(request, 'Invalid data entry')
+        return HttpResponseRedirect('/students/')
